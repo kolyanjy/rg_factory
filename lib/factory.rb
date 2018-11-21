e857011f7fd65e3951c12f68818d54a9381afb54
Nickolay Kolesnikov <kolyan654d@gmail.com>
2018-11-21 19:51:08 +0200

    add class Factory

--- lib/factory.rb
+++ lib/factory.rb
@@ -14,3 +14,82 @@
 # - to_a
 # - values_at
 # - ==, eql?
+
+class Factory
+  class << self
+    def new(*factory_args, &method)
+      const_set(factory_args.shift.capitalize, create_class(*factory_args, &method)) if factory_args.first.is_a? String
+      create_class(*factory_args, &method)
+    end
+
+    def create_class(*factory_args, &method)
+      Class.new do
+        attr_accessor *factory_args
+
+        define_method :initialize do |*arg_from_new_class|
+          raise ArgumentError, 'Extra arguments passed' unless factory_args.count == arg_from_new_class.count
+
+          factory_args.zip(arg_from_new_class).to_h.each do |variable, value|
+            instance_variable_set("@#{variable}", value)
+          end
+        end
+
+        define_method :[]= do |variable, value|
+          return instance_variable_set(instance_variables[variable]), value if variable.is_a? Integer
+
+          instance_variable_set("@#{variable}", value)
+        end
+
+        define_method :[] do |variable|
+          return instance_variable_get(instance_variables[variable]) if variable.is_a? Integer
+
+          instance_variable_get("@#{variable}")
+        end
+
+        define_method :== do |other|
+          self.class == other.class && to_a == other.to_a
+        end
+
+        define_method :each do |&method|
+          to_a.each &method
+        end
+
+        define_method :select do |&method|
+          to_a.select &method
+        end
+
+        define_method :values_at do |*index|
+          to_a.values_at *index
+        end
+
+        define_method :to_a do
+          instance_variables.map { |values| instance_variable_get values }
+        end
+
+        define_method :size do
+          instance_variables.count
+        end
+
+        define_method :members do
+          to_h.keys
+        end
+
+        define_method :each_pair do |&method|
+          to_h.each_pair &method
+        end
+
+        define_method :dig do |*arr|
+          arr.reduce(to_h) { |memo, char| (memo[char].is_a? NilClass) ? (return nil) : (memo[char]) }
+        end
+
+        define_method :to_h do
+          factory_args.zip(to_a).to_h
+        end
+
+        class_eval &method if block_given?
+        alias_method :length, :size
+        alias_method :eql?, :==
+      end
+    end
+  end
+end
